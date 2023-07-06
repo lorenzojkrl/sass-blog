177f0b1c20de3b69a21a00d9f9a9bf453a0172c0
Lorenzo <lorenzozar@gmail.com>
2023-07-06 15:07:34 +0300

    fix OAuth timeout error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+// Fix Nextjs Auth0 Discovery Timeout Error: https://community.auth0.com/t/nextjs-auth0-discovery-timeout-error/96667
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['s.gravatar.com']
+  }
 }
 
 module.exports = nextConfig
