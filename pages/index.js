94a7f6d8f1b307197e3be356f55b7cf37d1d99b4
Lorenzo <lorenzozar@gmail.com>
2023-07-06 15:09:21 +0300

    Landing Page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
+import Image from "next/image";
+import HeroImage from '../public/hero.webp'
+import { Logo } from "../components/logo";
+import Link from "next/link";
+
 export default function Home() {
-  return <div />;
+
+
+  return (
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" fill className="absolute" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+        <Logo />
+        <p>
+          Some text on this sass product.
+        </p>
+        <Link
+          href='/post/new'
+          className="bg-green-500 trackinf-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+        > Begin</Link>
+      </div>
+      <div>
+        <div>
+          PW: Auth0test
+        </div>
+      </div>
+    </div>
+  );
 }
