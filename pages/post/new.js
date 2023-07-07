8a2767734af6b7c2c65900068d8f72797c87106f
Lorenzo <lorenzozar@gmail.com>
2023-07-07 18:38:55 +0300

    Generate new post page

---
+++ pages/post/new.js
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+import { useRouter } from "next/router";
+
+export default function NewPost(props) {
+  const [topic, setTopic] = useState("");
+  const [keywords, setKeywords] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/generatePost", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ topic, keywords }),
+    });
+    const json = await response.json();
+    console.log("json", json);
+
+    if (json?.postId) {
+      router.push(`/post/${json.postId}`);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            <strong>Generate a blog post on:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block m-2 px-4 py-2 rounded-sm"
+            value={topic}
+            onChange={(e) => setTopic(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>
+            <strong>Targeting the following keywords:</strong>
+          </label>
+          <textarea
+            className="resize-none border border-slate-500 w-full block m-2 px-4 py-2 rounded-sm"
+            value={keywords}
+            onChange={(e) => setKeywords(e.target.value)}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-green-500 trackinf-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+        >
+          Generate
+        </button>
+      </form>
+    </div>
+  );
+}
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+  return <AppLayout {...pageProps}>{page}</AppLayout>;
+};
+
+export const getServerSideProps = withPageAuthRequired(() => {
+  return {
+    props: {},
+  };
+});
