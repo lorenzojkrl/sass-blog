00c3debeb625bbd9f1acb7a1451fb68af14e52eb
Lorenzo <lorenzozar@gmail.com>
2023-07-14 17:36:38 +0300

    update Logo

--- components/logo/Logo.js
+++ components/logo/Logo.js
@@ -1,14 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBrain } from "@fortawesome/free-solid-svg-icons"
+import { faFeather } from "@fortawesome/free-solid-svg-icons";
 
 export const Logo = () => {
   return (
-    <div className="text-3xl text-center py-4 font-heading">
-      BlogStandard
+    <div className="text-3xl text-center py-4 min-w-full min-w-10">
+      AI SEO Writer
       <FontAwesomeIcon
-        icon={faBrain}
-        className="text-2xl text-slate-400">
-      </FontAwesomeIcon>
+        icon={faFeather}
+        className="text-2xl text-white px-2"
+      ></FontAwesomeIcon>
     </div>
-  )
-} 
+  );
+};
