713cec215af6372862b8f4d2424bb970271dc939
Lorenzo <lorenzozar@gmail.com>
2023-08-05 12:47:01 +0300

    setup next-translate, i18n index

---
+++ i18n.js
@@ -0,0 +1,7 @@
+module.exports = {
+  locales: ["en", "es", "it"],
+  defaultLocale: "en",
+  pages: {
+    "*": ["common"], // Namespaces that you want to import per page
+  },
+};
--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 // Fix Nextjs Auth0 Discovery Timeout Error: https://community.auth0.com/t/nextjs-auth0-discovery-timeout-error/96667
 const dns = require("dns");
 dns.setDefaultResultOrder("ipv4first");
+const nextTranslate = require("next-translate-plugin");
 
-module.exports = {
+module.exports = nextTranslate({
   reactStrictMode: true,
   images: {
     domains: ["s.gravatar.com"],
@@ -12,4 +13,4 @@ module.exports = {
     locales: ["en", "es", "it"],
     defaultLocale: "en",
   },
-};
+});
