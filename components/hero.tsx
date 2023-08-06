a5f773840e48f6dfa66d5e00ca732bc885da0ba7
Lorenzo <lorenzozar@gmail.com>
2023-08-06 12:29:08 +0300

    i18n Hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,19 +2,21 @@ import Image from "next/image";
 import Container from "./container";
 import heroImg from "../public/img/hero.png";
 import Link from "next/link";
+import useTranslation from "next-translate/useTranslation";
 
 const Hero = () => {
+  const { t } = useTranslation("common");
+
   return (
     <>
       <Container className="flex flex-wrap ">
         <div className="flex items-center w-full lg:w-1/2">
           <div className="max-w-2xl mb-8">
             <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg:text-4xl lg:leading-tight xl:text-6xl xl:leading-tight dark:text-white">
-              Get To Your First Draft - Faster
+              {t("hero_title")}
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              AI SEO Writer is a content writing tool that helps you get to your
-              first draft faster.
+              {t("hero_subtitle")}
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
@@ -22,7 +24,7 @@ const Hero = () => {
                 href="/post/new"
                 className="px-8 py-2 text-lg font-medium text-center text-white bg-indigo-700 rounded-md "
               >
-                Get 10 free drafts
+                {t("hero_cta")}
               </Link>
             </div>
           </div>
