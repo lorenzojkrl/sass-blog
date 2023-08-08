af843cdff48aaf35b9e4d9e30d9cba5ab59af3b4
Lorenzo <lorenzozar@gmail.com>
2023-08-08 10:34:41 +0300

    fix incorrect path

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import { DM_Sans, DM_Serif_Display } from "next/font/google";
 import { PostsProvider } from "../context/postsContext";
 import { MantineProvider } from "@mantine/core";
 import Head from "next/head";
-import LanguageSetter from "../components/sha";
+import LanguageSetter from "../components/shared/languageSetter";
 // Fix fontawesome huge icon on startup
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
