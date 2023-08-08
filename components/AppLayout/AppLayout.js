9058e5f1b8b1a1a17b3f9faf350410a410c785c0
Lorenzo <lorenzozar@gmail.com>
2023-08-08 13:50:58 +0300

    Test fix using media queries

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -2,8 +2,6 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { Logo } from "../logo";
 import { useContext, useEffect } from "react";
 import PostsContext from "../../context/postsContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFeather } from "@fortawesome/free-solid-svg-icons";
 import {
   createStyles,
   Divider,
@@ -11,16 +9,12 @@ import {
   Drawer,
   ScrollArea,
   rem,
-  Header,
-  Group,
-  Title,
-  Burger,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import SidebarFooter from "./sidebarFooter";
 import SidebarLoadMore from "./sidebarLoadMore";
 import SidebarHeader from "./sidebarHeader";
-// import MobileHeader from "../mobileHeader";
+import MobileHeader from "../mobileHeader";
 import useTranslation from "next-translate/useTranslation";
 
 const useStyles = createStyles((theme) => ({
@@ -115,105 +109,77 @@ export const AppLayout = ({
 
   return (
     <>
-      {user && (
-        <div className="block md:hidden">
-          <div className="w-full">
-            <nav className="container relative  flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
-              <Box className="lg:pb-20" style={{ width: "100%" }}>
-                {/* <MobileHeader
+      <div className="block hide-on-large-screens">
+        <div className="w-full">
+          <nav className="container relative  flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+            <Box className="lg:pb-20" style={{ width: "100%" }}>
+              <MobileHeader
                 drawerOpened={drawerOpened}
                 toggleDrawer={toggleDrawer}
                 style={classes.hiddenDesktop}
-              /> */}
-                <Header height={60} px="md ">
-                  <Group
-                    position="apart"
-                    className="w-full"
-                    sx={{ height: "100%" }}
-                  >
-                    <Group>
-                      <Title order={2} className="text-slate-900/90">
-                        AI SEO Writer
-                      </Title>
+              />
 
-                      <FontAwesomeIcon
-                        icon={faFeather}
-                        size="lg"
-                        className=" text-slate-900/90"
-                      ></FontAwesomeIcon>
-                    </Group>
+              <Drawer
+                opened={drawerOpened}
+                onClose={closeDrawer}
+                size="100%"
+                padding="md"
+                title={t("aiseowriter")}
+                className={classes.hiddenDesktop}
+                zIndex={1000000}
+              >
+                <ScrollArea style={{ overflowX: "hidden" }}>
+                  <Divider my="sm" color="gray.1" />
+                  <SidebarHeader
+                    availableTokens={availableTokens}
+                    closeDrawer={closeDrawer}
+                  ></SidebarHeader>
+                  <Divider my="sm" color="gray.1" />
+                  <SidebarLoadMore
+                    noMorePosts={noMorePosts}
+                    getPosts={getPosts}
+                    posts={posts}
+                    postId={postId}
+                    closeDrawer={closeDrawer}
+                  ></SidebarLoadMore>
+                  <Divider my="sm" color="gray.1" />
+                  <SidebarFooter user={user}></SidebarFooter>
+                </ScrollArea>
+              </Drawer>
+            </Box>
+          </nav>
+        </div>
+        {children}
+      </div>
 
-                    <Burger
-                      opened={drawerOpened}
-                      onClick={toggleDrawer}
-                      className={classes.hiddenDesktop}
-                    />
-                  </Group>
-                </Header>
-
-                <Drawer
-                  opened={drawerOpened}
-                  onClose={closeDrawer}
-                  size="100%"
-                  padding="md"
-                  title={t("aiseowriter")}
-                  className={classes.hiddenDesktop}
-                  zIndex={1000000}
-                >
-                  {/* <ScrollArea style={{ overflowX: "hidden" }}>
-                    <Divider my="sm" color="gray.1" />
-                    <SidebarHeader
-                      availableTokens={availableTokens}
-                      closeDrawer={closeDrawer}
-                    ></SidebarHeader>
-                    <Divider my="sm" color="gray.1" />
-                    <SidebarLoadMore
-                      noMorePosts={noMorePosts}
-                      getPosts={getPosts}
-                      posts={posts}
-                      postId={postId}
-                      closeDrawer={closeDrawer}
-                    ></SidebarLoadMore>
-                    <Divider my="sm" color="gray.1" />
-                    <SidebarFooter user={user}></SidebarFooter>
-                  </ScrollArea> */}
-                </Drawer>
-              </Box>
-            </nav>
+      <div className="hide-on-small-screens">
+        <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
+          <div className="flex flex-col text-white overflow-hidden">
+            <div className="bg-slate-800 px-2 ">
+              <Logo />
+              <Divider my="sm" color="rgb(30 41 59)" />
+              <SidebarHeader
+                availableTokens={availableTokens}
+                closeDrawer={closeDrawer}
+              ></SidebarHeader>
+              <Divider my="sm" color="rgb(30 41 59)" />
+            </div>
+            <div className="flex-1 bg-gradient-to-b from-slate-800 to-cyan-800">
+              <SidebarLoadMore
+                noMorePosts={noMorePosts}
+                getPosts={getPosts}
+                posts={posts}
+                postId={postId}
+                closeDrawer={closeDrawer}
+              ></SidebarLoadMore>
+            </div>
+            <div className="pt-2 bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
+              <SidebarFooter user={user}></SidebarFooter>
+            </div>
           </div>
           {children}
         </div>
-      )}
-      {user && (
-        <div className="hidden md:block">
-          <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
-            <div className="flex flex-col text-white overflow-hidden">
-              <div className="bg-slate-800 px-2 ">
-                <Logo />
-                <Divider my="sm" color="rgb(30 41 59)" />
-                <SidebarHeader
-                  availableTokens={availableTokens}
-                  closeDrawer={closeDrawer}
-                ></SidebarHeader>
-                <Divider my="sm" color="rgb(30 41 59)" />
-              </div>
-              <div className="flex-1 bg-gradient-to-b from-slate-800 to-cyan-800">
-                <SidebarLoadMore
-                  noMorePosts={noMorePosts}
-                  getPosts={getPosts}
-                  posts={posts}
-                  postId={postId}
-                  closeDrawer={closeDrawer}
-                ></SidebarLoadMore>
-              </div>
-              <div className="pt-2 bg-cyan-800 flex items-center gap-2 border-t border-t-black/50 h-20 px-2">
-                <SidebarFooter user={user}></SidebarFooter>
-              </div>
-            </div>
-            {children}
-          </div>
-        </div>
-      )}
+      </div>
     </>
   );
 };
