f49e7ca01ec5807d4562dd4f2bf75adbba95d622
Lorenzo <lorenzozar@gmail.com>
2023-08-10 23:10:18 +0300

    initial prompts i18n for newsletter

--- i18n.js
+++ i18n.js
@@ -2,6 +2,6 @@ module.exports = {
   locales: ["en", "es", "it"],
   defaultLocale: "en",
   pages: {
-    "*": ["common", "seoFacts"], // Namespaces that you want to import per page
+    "*": ["common", "seoFacts", "prompts"],
   },
 };
