f3184aa522890603f257887ce865a11ac527248b
Lorenzo <lorenzozar@gmail.com>
2023-09-12 17:21:06 +0300

    use Figma design on navbar and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,76 +1,22 @@
-import Link from "next/link";
 import React from "react";
-import Container from "./container";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFeather } from "@fortawesome/free-solid-svg-icons";
 import useTranslation from "next-translate/useTranslation";
+import {
+  Text,
+} from "@mantine/core";
+import Container from "./container";
 
 export default function Footer() {
-  const navigationTest = ["features"];
-  const legalTest = [{ title: "startFree", url: "/post/new" }];
   const { t } = useTranslation("common");
 
   return (
-    <div className="relative">
-      <Container>
-        <div className="grid max-w-screen-xl grid-cols-1 gap-10 pt-10 mx-auto mt-5 border-t border-gray-100 lg-grid-cols-5">
-          <div className="lg-col-span-2">
-            <div>
-              <Link
-                href="/"
-                className="flex items-center space-x-2 text-2xl font-medium text-indigo-800"
-              >
-                <span>{t("aiseowriter")}</span>
-                <FontAwesomeIcon
-                  icon={faFeather}
-                  size="xs"
-                  className="px-2 text-indigo-500"
-                ></FontAwesomeIcon>
-              </Link>
-            </div>
-
-            <div className="max-w-md mt-4 text-gray-500 ">
-              {t("hero_subtitle")}
-            </div>
-          </div>
-
-          <div>
-            <div className="flex flex-wrap w-full -mt-2 -ml-3">
-              {navigationTest.map((item, index) => (
-                <Link
-                  key={index}
-                  href="/"
-                  className="w-full px-4 py-2 text-gray-500 rounded-md  hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none"
-                >
-                  {t(`${item}`)}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div>
-            <div className="flex flex-wrap w-full -mt-2 -ml-3">
-              {legalTest.map((item, index) => (
-                <Link
-                  key={index}
-                  href={item.url}
-                  className="w-full px-4 py-2 text-gray-500 rounded-md  hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none"
-                >
-                  {t(`${item.title}`)}
-                </Link>
-              ))}
-            </div>
-          </div>
-          <div className="">
-            <div>
-              <b>{t("G_contact")}</b> contact@aiseowriter.co
-            </div>
-          </div>
-        </div>
-
-        <div className="my-10 text-sm text-center text-gray-600">
-          Copyright © 2023 {/* {new Date().getFullYear()} */}. Made with ♥ by AI
-          SEO Writer.
-        </div>
+    <div className="relative bg-[#DEE2E6] h-[100px] lg:h-[144px] flex items-center">
+      <Container className="toggle-row-column toggle-p-font-size ">
+        <Text color="gray.6">
+          {t("G_contact", { email: "" })} <a href="mailto:someone@yoursite.com">contact@aiseowriter.co</a>
+        </Text>
+        <Text color="gray.6">
+          Copyright © {new Date().getFullYear()}. Made with ♥ by AI SEO Writer.
+        </Text>
       </Container>
     </div>
   );
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,88 +2,18 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFeather } from "@fortawesome/free-solid-svg-icons";
 import {
-  createStyles,
-  Header,
   HoverCard,
   Group,
-  Button,
-  UnstyledButton,
   Text,
   SimpleGrid,
-  ThemeIcon,
-  Divider,
   Center,
   Box,
-  Burger,
-  Drawer,
-  ScrollArea,
-  rem,
-  Title,
-  Collapse,
 } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
 import {
-  IconBook,
-  IconCoin,
   IconChevronDown,
-  IconLanguage,
 } from "@tabler/icons";
 import useTranslation from "next-translate/useTranslation";
 import { setCookie } from "../utils/cookieUtils";
-
-const useStyles = createStyles((theme) => ({
-  link: {
-    display: "flex",
-    alignItems: "center",
-    height: "100%",
-    paddingLeft: theme.spacing.md,
-    paddingRight: theme.spacing.md,
-    textDecoration: "none",
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-    fontWeight: 500,
-    fontSize: theme.fontSizes.sm,
-
-    ...theme.fn.hover({
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[6]
-          : theme.colors.gray[0],
-    }),
-  },
-
-  subLink: {
-    width: "100%",
-    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
-    borderRadius: theme.radius.md,
-
-    ...theme.fn.hover({
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[7]
-          : theme.colors.gray[0],
-    }),
-
-    "&:active": theme.activeStyles,
-  },
-
-  dropdownFooter: {
-    backgroundColor:
-      theme.colorScheme === "dark"
-        ? theme.colors.dark[7]
-        : theme.colors.gray[0],
-    margin: `calc(${theme.spacing.md} * -1)`,
-    marginTop: theme.spacing.sm,
-    padding: `${theme.spacing.md} calc(${theme.spacing.md} * 2)`,
-    paddingBottom: theme.spacing.xl,
-    borderTop: `${rem(1)} solid ${
-      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[1]
-    }`,
-  },
-}));
-
-const freeCopies = 10;
-const copiesForTenUSD = 80;
-const copiesForHundredUSD = 1000;
 
 const supportedLanguages = [
   { name: "English", locale: "en" },
@@ -92,253 +22,74 @@ const supportedLanguages = [
 ];
 
 export function Navbar() {
-  const { t } = useTranslation("common");
-  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
-    useDisclosure(false);
-  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
-  const { classes, theme } = useStyles();
+  const { t, lang } = useTranslation("common");
   const handleLanguageChange = (locale: string) => {
     setCookie("language", locale, 365);
   };
 
-  const pricingData = [
-    {
-      icon: IconBook,
-      title: "pricingTitle_One",
-      description: "pricingDescription_One",
-      copies: freeCopies,
-    },
-    {
-      icon: IconCoin,
-      title: "pricingTitle_Two",
-      description: "pricingDescription_Two",
-      copies: copiesForTenUSD,
-    },
-    {
-      icon: IconCoin,
-      title: "pricingTitle_Three",
-      description: "pricingDescription_Three",
-      copies: copiesForHundredUSD,
-    },
-  ];
-
-  const pricingLinks = pricingData.map((item) => (
-    <UnstyledButton className={classes.subLink} key={item.title}>
-      <Group noWrap>
-        <ThemeIcon size={34} variant="default" radius="md">
-          <item.icon size={rem(22)} color={theme.fn.primaryColor()} />
-        </ThemeIcon>
-        <div>
-          <Text size="sm" fw={500}>
-            {t(item.title, { copies: item.copies })}
-          </Text>
-          <Text size="xs" color="dimmed">
-            {t(item.description, { copies: item.copies })}
-          </Text>
-        </div>
-      </Group>
-    </UnstyledButton>
-  ));
-
   return (
-    <div className="w-full">
-      <nav className="container relative  flex flex-wrap items-center justify-between p-8 mx-auto lg-justify-between">
-        <Box className="lg-pb-20" style={{ width: "100%" }}>
-          <Header height={60} px="md ">
-            <Group position="apart" className="w-full" sx={{ height: "100%" }}>
-              <Group>
-                <Title order={2} className="text-slate-900/90">
-                  {t("aiseowriter")}
-                </Title>
-
-                <FontAwesomeIcon
-                  icon={faFeather}
-                  size="lg"
-                  className=" text-slate-900/90"
-                ></FontAwesomeIcon>
-              </Group>
-              <Group
-                sx={{ height: "100%" }}
-                spacing={0}
-                className="hide-on-small-screens-flex"
-              >
-                <a href="#" className={classes.link}>
-                  {t("home")}
-                </a>
-
-                <a href="#" className={classes.link}>
-                  {t("features")}
-                </a>
-                <Link href="/post/new" className={classes.link}>
-                  {t("startFree")}
-                </Link>
-                <HoverCard
-                  width={600}
-                  position="bottom"
-                  radius="md"
-                  shadow="md"
-                  withinPortal
-                >
-                  <HoverCard.Target>
-                    <a href="#" className={classes.link}>
-                      <Center inline>
-                        <Box component="span" mr={5}>
-                          {t("pricing")}
-                        </Box>
-                        <IconChevronDown
-                          size={16}
-                          color={theme.fn.primaryColor()}
-                        />
-                      </Center>
-                    </a>
-                  </HoverCard.Target>
-
-                  <HoverCard.Dropdown sx={{ overflow: "hidden" }}>
-                    <Group position="apart" px="md">
-                      <Text fw={500}>{t("starterPricing")}</Text>
-                    </Group>
-
-                    <Divider
-                      my="sm"
-                      mx="-md"
-                      color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
-                    />
-
-                    <SimpleGrid cols={2} spacing={0}>
-                      {pricingLinks}
-                    </SimpleGrid>
-
-                    <div className={classes.dropdownFooter}>
-                      <Group position="apart">
-                        <div>
-                          <Text fw={500} fz="sm">
-                            {t("startFree")}
-                          </Text>
-                        </div>
-                        <Button variant="default">
-                          <Link href="/post/new">{t("G_getStarted")}</Link>
-                        </Button>
-                      </Group>
-                    </div>
-                  </HoverCard.Dropdown>
-                </HoverCard>
-              </Group>
-              <Group className="hide-on-small-screens-flex">
-                <Link
-                  href="/post/new"
-                  className="px-8 py-2 text-lg font-medium text-center text-white bg-indigo-700 rounded-md "
-                >
-                  {t("G_logIn")}
-                </Link>
-                <Link
-                  href="/post/new"
-                  className="px-8 py-2 text-lg font-medium text-center text-white bg-indigo-700 rounded-md "
-                >
-                  {t("G_signUp")}
-                </Link>
-                <HoverCard
-                  width={150}
-                  position="bottom"
-                  radius="md"
-                  shadow="md"
-                  withinPortal
-                >
-                  <HoverCard.Target>
-                    <a href="#" className={classes.link}>
-                      <Center inline>
-                        <IconLanguage size={28} color="rgb(67 56 202)" />
-                      </Center>
-                    </a>
-                  </HoverCard.Target>
-
-                  <HoverCard.Dropdown sx={{ overflow: "hidden" }}>
-                    <SimpleGrid cols={1}>
-                      {supportedLanguages.map((language) => (
-                        <Link
-                          href="/"
-                          locale={language.locale}
-                          key={language.locale}
-                          className="text-right"
-                          onClick={() => handleLanguageChange(language.locale)}
-                        >
-                          {language.name}
-                        </Link>
-                      ))}
-                    </SimpleGrid>
-                  </HoverCard.Dropdown>
-                </HoverCard>
-              </Group>
-
-              {/* <Burger
-                opened={drawerOpened}
-                onClick={toggleDrawer}
-                className={classes.hiddenDesktop}
-              /> */}
+    <div className="w-full bg-[#353B41]">
+      <nav className="container relative flex flex-wrap items-center justify-between py-8 lg:py-16 px-8 mx-auto lg-justify-between">
+        <Box style={{ width: "100%" }}>
+          <Group position="apart" className="w-full text-white bg-[#353B41]" sx={{ height: "100%" }}>
+            <Group>
+              <FontAwesomeIcon
+                icon={faFeather}
+                className="toggle-header-font-size"
+              ></FontAwesomeIcon>
+              <Text className="toggle-header-font-size" fw={700}>
+                {t("aiseowriter")}
+              </Text>
             </Group>
-          </Header>
+            <Group >
+              <HoverCard
+                width={150}
+                position="bottom"
+                radius="md"
+                shadow="md"
+                withinPortal
+              >
+                <HoverCard.Target>
+                  <Center className="auth-btn bg-[#212529] flex w-[100px] uppercase">
+                    <Text className="text-[12px] lg:text-[16px]" mr={10}>{lang}</Text>
+                    <IconChevronDown
+                      size={16}
+                    />
+                  </Center>
+                </HoverCard.Target>
 
-          {/* <Drawer
-            opened={drawerOpened}
-            onClose={closeDrawer}
-            size="100%"
-            padding="md"
-            title="Navigation"
-            className={classes.hiddenDesktop}
-            zIndex={1000000}
-          >
-            <ScrollArea h={`calc(100vh - ${rem(60)})`} mx="-md">
-              <Divider
-                my="sm"
-                color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
-              />
-
-              <a href="#" className={classes.link}>
-                {t("home")}
-              </a>
-
-              <a href="#" className={classes.link}>
-                {t("features")}
-              </a>
-              <Link href="/post/new" className={classes.link}>
-                {t("startFree")}
+                <HoverCard.Dropdown sx={{ overflow: "hidden" }} className="bg-[#212529] border-none text-white">
+                  <SimpleGrid cols={1}>
+                    {supportedLanguages.map((language) => (
+                      <Link
+                        href="/"
+                        locale={language.locale}
+                        key={language.locale}
+                        className="text-right"
+                        onClick={() => handleLanguageChange(language.locale)}
+                      >
+                        {language.name}
+                      </Link>
+                    ))}
+                  </SimpleGrid>
+                </HoverCard.Dropdown>
+              </HoverCard>
+              <Link
+                href="/post/new"
+                className="auth-btn bg-[#212529] hide-on-small-screens"
+              >
+                {t("G_logIn")}
               </Link>
-
-              <UnstyledButton className={classes.link} onClick={toggleLinks}>
-                <Center inline>
-                  <a href="#">
-                    <IconLanguage size={22} color="#000" />
-                  </a>
-                </Center>
-              </UnstyledButton>
-              <Collapse in={linksOpened}>
-                <SimpleGrid cols={1}>
-                  {supportedLanguages.map((language) => (
-                    <Link
-                      href="/"
-                      locale={language.locale}
-                      key={language.locale}
-                      className={classes.link}
-                      onClick={() => handleLanguageChange(language.locale)}
-                    >
-                      {language.name}
-                    </Link>
-                  ))}
-                </SimpleGrid>
-              </Collapse>
-
-              <Divider
-                my="sm"
-                color={theme.colorScheme === "dark" ? "dark.5" : "gray.1"}
-              />
-
-              <Group position="center" grow pb="xl" px="md">
-                <Link href="/post/new">{t("G_signUp")}</Link>
-                <Link href="/post/new">{t("G_logIn")}</Link>
-              </Group>
-            </ScrollArea>
-          </Drawer> */}
+              <Link
+                href="/post/new"
+                className="auth-btn hide-on-small-screens"
+              >
+                {t("G_signUp")}
+              </Link>
+            </Group>
+          </Group>
         </Box>
       </nav>
-    </div>
+    </div >
   );
 }
