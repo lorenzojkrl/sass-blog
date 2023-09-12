3731b3cfe651450b8bfe5676ae20f929b8605bf1
Lorenzo <lorenzozar@gmail.com>
2023-09-12 20:56:54 +0300

    Hero follows Figma design

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import Container from "./container";
-import heroImg from "../public/img/hero.png";
 import Link from "next/link";
 import useTranslation from "next-translate/useTranslation";
 
@@ -8,40 +6,56 @@ const Hero = () => {
   const { t } = useTranslation("common");
 
   return (
-    <Container className="flex flex-wrap ">
-      <div className="flex items-center w-full lg-halveWidth ">
-        <div className="max-w-2xl mb-8">
-          <h1 className="text-4xl font-bold leading-snug tracking-tight text-gray-800 lg-heading">
-            {t("hero_title")}
-          </h1>
-          <p className="py-5 text-xl leading-normal text-gray-500 lg-text-xl">
-            {t("hero_subtitle")}
-          </p>
+    <div className="w-full bg-gray-10">
+      <Container className="flex flex-wrap">
+        <div className="flex items-center w-full lg-halveWidth ">
+          <div className="max-w-[500px] mb-8">
+            <h1 className="lg:text-[64px] font-bold leading-snug tracking-tight text-white">
+              {t("hero_title")} <span style={{ color: 'var(--cyan-7)' }}>{t("hero_title_seconds")}</span>
+            </h1>
+            <p className="lg:py-5 text-gray-5 lg-text-xl">
+              {t("hero_subtitle")}
+            </p>
 
-          <div className="flex flex-col items-start">
-            <Link
-              href="/post/new"
-              className="px-8 py-2 text-lg font-medium text-center text-white bg-indigo-700 rounded-md "
-            >
-              {t("hero_cta")}
-            </Link>
+            <div className="flex justify-between my-8">
+              <Link
+                href="/post/new"
+                className="auth-btn hide-on-large-screens w-[175px]"
+              >
+                {t("G_signUp")}
+              </Link>
+              <Link
+                href="/post/new"
+                className="auth-btn bg-gray-9 hide-on-large-screens w-[175px]"
+              >
+                {t("G_logIn")}
+              </Link>
+            </div>
+            <div className="flex flex-col items-start lg:pt-8 hide-on-small-screens">
+              <Link
+                href="/post/new"
+                className="auth-btn h-[65px] w-[245px] text-[28px] font-semibold rounded-md"
+              >
+                {t("hero_cta")}
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center w-full lg-halveWidth ">
-        <div style={{ position: "relative", width: "500px" }}>
-          <Image
-            src={heroImg}
-            className={"object-cover"} // check this
-            alt="Woman Working On Computer
-              BY PAWEL on https://www.glazestock.com/"
-            loading="eager"
-            placeholder="blur"
-            style={{ zIndex: "-1", objectFit: "contain" }}
-          />
+        <div className="flex w-full lg-halveWidth">
+          <div className="w-[570px] h-[700px] border border-dark-gray rounded-lg text-gray-4 bg-gray-7 p-4 lg:p-8">
+            <div className="py-1 px-3 inline-block max-w-content bg-dark-gray-1 rounded-md text-center text-[14px] lg:text-[16px] text-cyan-5 font-bold">
+              {t("demoTagline")}
+            </div>
+            <h1 className="demo-h1">{t("demoTitle")}</h1>
+            <p className="demo-p ">{t("demoIntro")}</p>
+            <h2 className="demo-h2">{t("demoH2_First")}</h2>
+            <p className="demo-p">{t("demoP_First")}</p>
+            <h2 className="demo-h2">{t("demoH2_Second")}</h2>
+            <p className="demo-p">{t("demoP_Second")}</p>
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
